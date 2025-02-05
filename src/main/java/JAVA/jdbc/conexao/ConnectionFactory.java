9c074a7184d5039c383419642a3ed10d44e649b6
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-02-05 16:12:59 -0300

    conectando no banco de dados

--- src/main/java/JAVA/jdbc/conexao/ConnectionFactory.java
+++ src/main/java/JAVA/jdbc/conexao/ConnectionFactory.java
@@ -1,7 +1,22 @@
 package JAVA.jdbc.conexao;
 
-public class ConnectionFactory {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+    private static final String url = "jdbc:mysql://localhost:3307/devdojo_maratona";
+    private static final String username = "root";
+    private static final String password = "root";
+    public static Connection GetConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("erro ao conectar no banco de dados, erro" + e);
+        }
+        return null;
     }
 }
+
---
+++ src/main/java/JAVA/jdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,11 @@
+package JAVA.jdbc.test;
+
+import JAVA.jdbc.conexao.ConnectionFactory;
+
+import java.sql.Connection;
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args) {
+        ConnectionFactory.GetConnection();
+    }
+}
